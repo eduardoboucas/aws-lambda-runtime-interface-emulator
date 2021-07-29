d59471987ffddb0007f9d5aba3a08e7da7b56aed
Eduardo Bouças <mail@eduardoboucas.com>
2021-07-29 09:47:21 +0100

    feat: add InvokedFunctionArn property to standalone handler (#3)
    * feat: add InvokedFunctionArn property to standalone handler
    
    * refactor: change getFunctionArn signature

--- lambda/rapidcore/standalone/executeHandler.go
+++ lambda/rapidcore/standalone/executeHandler.go
@@ -5,19 +5,32 @@ package standalone
 
 import (
 	"net/http"
+	"strings"
 
 	log "github.com/sirupsen/logrus"
 	"go.amzn.com/lambda/interop"
 	"go.amzn.com/lambda/rapidcore"
 )
 
-func Execute(w http.ResponseWriter, r *http.Request, sandbox rapidcore.Sandbox) {
+// Extracting the invoked function ARN from the FunctionName request parameter
+// https://docs.aws.amazon.com/lambda/latest/dg/API_Invoke.html#API_Invoke_RequestSyntax
+func getFunctionArn(urlPath string) string {
+	nodes := strings.Split(urlPath, "/")
 
+	if len(nodes) != 5 {
+		return ""
+	}
+
+	return nodes[3]
+}
+
+func Execute(w http.ResponseWriter, r *http.Request, sandbox rapidcore.Sandbox) {
 	invokePayload := &interop.Invoke{
-		TraceID:         r.Header.Get("X-Amzn-Trace-Id"),
-		LambdaSegmentID: r.Header.Get("X-Amzn-Segment-Id"),
-		Payload:         r.Body,
-		CorrelationID:   "invokeCorrelationID",
+		TraceID:            r.Header.Get("X-Amzn-Trace-Id"),
+		LambdaSegmentID:    r.Header.Get("X-Amzn-Segment-Id"),
+		Payload:            r.Body,
+		CorrelationID:      "invokeCorrelationID",
+		InvokedFunctionArn: getFunctionArn(r.URL.Path),
 	}
 
 	// If we write to 'w' directly and waitUntilRelease fails, we won't be able to propagate error anymore
